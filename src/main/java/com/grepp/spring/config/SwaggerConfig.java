047521cda23b8a43933d315dc426bb486dd465dc
Leeka99 <dsrn0000@gmail.com>
2025-07-08 15:00:08 +0900

    lkh

--- src/main/java/com/grepp/spring/config/SwaggerConfig.java
+++ src/main/java/com/grepp/spring/config/SwaggerConfig.java
@@ -1,15 +1,15 @@
 package com.grepp.spring.config;
 
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.IntegerSchema;
 import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,19 +20,28 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApiSpec() {
-        return new OpenAPI().components(new Components()
-                .addSchemas("ApiErrorResponse", new ObjectSchema()
-                        .addProperty("status", new IntegerSchema())
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("fieldErrors", new ArraySchema().items(
-                                new Schema<ArraySchema>().$ref("ApiFieldError"))))
-                .addSchemas("ApiFieldError", new ObjectSchema()
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("property", new StringSchema())
-                        .addProperty("rejectedValue", new ObjectSchema())
-                        .addProperty("path", new StringSchema())));
+        return new OpenAPI()
+            .info(new Info()
+                .title("이때어때 API 문서")
+                .description("이때어때 API 명세입니다. 에러 코드는 [Error Code 문서](https://www.notion.so/Error-Code-22915a0120548059a805d7c6027820d9?source=copy_link)를 참조해 주세요.")
+                .version("v1.0.0"))
+            .components(
+                new Components()
+                    .addSecuritySchemes("bearerAuth"
+                        , new SecurityScheme()
+                            .name("bearerAuth")
+                            .type(
+                                SecurityScheme.Type.HTTP)
+                            .scheme(
+                                "bearer")
+                            .bearerFormat(
+                                "JWT")
+                            .description(
+                                "JWT 토큰을 입력하세요. Bearer 는 생략하세요")
+                    ))
+            .addServersItem(new Server().url("/"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+
     }
 
     @Bean
@@ -40,11 +49,10 @@ public class SwaggerConfig {
         // add error type to each operation
         return (operation, handlerMethod) -> {
             operation.getResponses().addApiResponse("4xx/5xx", new ApiResponse()
-                    .description("Error")
-                    .content(new Content().addMediaType("*/*", new MediaType().schema(
-                            new Schema<MediaType>().$ref("ApiErrorResponse")))));
+                .description("Error")
+                .content(new Content().addMediaType("*/*", new MediaType().schema(
+                    new Schema<MediaType>().$ref("ApiErrorResponse")))));
             return operation;
         };
     }
-
 }
