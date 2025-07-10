72ce8b53e030a628d12d0722c9c743af1ffe2a54
Leeka99 <dsrn0000@gmail.com>
2025-07-11 01:01:39 +0900

    feat: schedule

--- src/main/java/com/grepp/spring/app/controller/api/schedules/payload/ShowSuggestedLocationsResponse.java
+++ src/main/java/com/grepp/spring/app/controller/api/schedules/payload/ShowSuggestedLocationsResponse.java
@@ -3,6 +3,7 @@ package com.grepp.spring.app.controller.api.schedules.payload;
 import com.grepp.spring.app.model.middle_region.domain.MiddleRegion;
 import com.grepp.spring.app.model.schedule.domain.VOTE_STATUS;
 import java.util.List;
+import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,12 +13,14 @@ public class ShowSuggestedLocationsResponse {
     private String locationName;
     private Double latitude;
     private	Double longitude;
-    private	Long suggestedMemberId;
-    private Long voteCount;
+//    private	Long suggestedMemberId;
+//    private Long voteCount;
 //    private VOTE_STATUS SCHEDULES_STATUS;
 
-    private List<String> metroLines; // 추가
-    private List<String> stationColors; // 추가
+    private Map<String, String> metroLines;  // (호선, 호선 색)
+
+//    private List<String> metroLines; // 추가
+//    private List<String> stationColors; // 추가
 //    private int noVoteCount; // 추가. 투표하지 않은 사람 수
 
 }
